d6572c855c574a438d117f41fd0452b52cc20c11
Michael Dinata <michaeldinata98@gmail.com>
2024-06-29 01:45:32 +0800

    FOOD-9: adding material UI modal to popup when find food button is clicked

--- src/App.js
+++ src/App.js
@@ -3,11 +3,36 @@ import './App.css';
 import { api } from './index'
 import { Link } from "react-router-dom";
 import { Component } from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
 
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showFoodModalIsOpen: false
+    }
+  }
+
   componentDidMount() {
     this.getSession();
+  }
+
+  setShowFoodModalState(isOpen) {
+    this.setState({ showFoodModalIsOpen: isOpen })
   }
 
   getCSRF() {
@@ -82,6 +107,7 @@ class App extends Component {
                     };
 
                     this.handleFindFood(pos)
+                    this.setShowFoodModalState(true)
                   },
                   () => {
                     console.log("browser doesn't support geolocation")
@@ -90,8 +116,22 @@ class App extends Component {
               }
             }}>
             Find Food For Me!
+            
           </button>
+          
         </header>
+        <Modal
+            open={this.state.showFoodModalIsOpen}
+            onClose={() => this.setShowFoodModalState(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyle}>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              test
+            </Typography>
+            </Box>
+          </Modal>
       </div>
     );
   }
